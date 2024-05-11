42658bf6e4d4168543e564b4f22a86e2d68b11a0
trrabby1@gmail.com <110276063+trrabby@users.noreply.github.com>
2024-05-12 00:11:17 +0600

    added wishlist

--- index.js
+++ index.js
@@ -46,17 +46,96 @@ async function run() {
 
     app.get('/blogs', async (req, res) => {
       const cursor = itemCollection.find()
-      const result = await cursor.toArray();
-      res.send(result)
+      try {
+        const result = await cursor.toArray();
+        res.send(result)
+      }
+      catch (error) {
+        console.log(error)
+      }
+
     })
 
     app.post('/blogs', async (req, res) => {
       const item = req.body;
       // console.log(user, "from server")
-      const result = await itemCollection.insertOne(item);
-      res.send(result);
+      try {
+        const result = await itemCollection.insertOne(item);
+        res.send(result);
+      }
+      catch (err) {
+        console.log(err)
+      }
+
 
     });
+
+
+    app.post('/wishlist', async (req, res) => {
+      const item = req.body;
+      // console.log(user, "from server")
+
+      try {
+        const result = await itemCollection2.insertOne(item);
+        res.send(result);
+      }
+      catch (error) {
+        console.log(error.errmsg)
+      }
+
+    });
+
+    app.get('/wishlist', async (req, res) => {
+      const cursor = itemCollection2.find()
+      try {
+        const result = await cursor.toArray();
+        res.send(result)
+      }
+      catch (error) {
+        console.log(error)
+      }
+
+    })
+
+    app.get('/wishlists/:id', async (req, res) => {
+      
+      try {
+        const result = await itemCollection2.findOne({ _id: new ObjectId(req.params.id) });
+        res.send(result)
+      }
+      catch (err) {
+        console.log(err)
+      }
+
+    })
+
+    app.get('/wishlist/:email', async (req, res) => {
+      
+      try {
+        const result = await itemCollection2.find({ wishListersEmail: req.params.email }).toArray();
+        res.send(result)
+      }
+      catch (err) {
+        console.log(err)
+      }
+
+    })
+
+    app.delete('/wishlists/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      try {
+        const result = await itemCollection2.deleteOne(query);
+        res.send(result);
+      }
+      catch (err) {
+        console.log(err)
+      }
+
+    })
+
+
 
     app.get('/blogs/:id', async (req, res) => {
       const id = req.params.id;
@@ -65,17 +144,45 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/blogs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      try {
+        const result = await itemCollection.deleteOne(query);
+        res.send(result);
+      }
+      catch (err) {
+        console.log(err)
+      }
+
+    })
+
     app.get('/blogs-cat/:category', async (req, res) => {
       console.log(req.params.category)
-      const result = await itemCollection.find({ category: req.params.category }).toArray();
-      res.send(result)
+
+      try {
+        const result = await itemCollection.find({ category: req.params.category }).toArray();
+        res.send(result)
+      }
+      catch (err) {
+        console.log(err)
+      }
+
     })
 
     /* API to search text from title */
     app.get('/blogs-head/:title', async (req, res) => {
       const text = (req.params.title)
-      const result = await itemCollection.find({ title: new RegExp(text, 'i')}).toArray();
-      res.send(result)
+
+      try {
+        const result = await itemCollection.find({ title: new RegExp(text, 'i') }).toArray();
+        res.send(result)
+      }
+      catch (err) {
+        console.log(err)
+      }
+
     })
 
     app.put('/blogs/:id', async (req, res) => {
@@ -88,15 +195,21 @@ async function run() {
           ...updateData,
         },
       };
-      const result = await itemCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      try {
+        const result = await itemCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+      }
+      catch (err) {
+        console.log(err)
+      }
+
     })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
-  
+  }
+
   finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
